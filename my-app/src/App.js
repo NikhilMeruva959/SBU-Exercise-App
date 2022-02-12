7bf4da90ce51c0e3315efb00a2ced3788b3f3df7
Nikhil Meruva <nikmeruva1@gmail.com>
2022-02-12 13:09:22 -0500

    Building out Login Form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,14 @@ import AuthContext from './components/auth-context/auth-context';
 import './App.css';
 import React, { useContext } from 'react';
 import MainHeader from './components/Header/Header';
+import Login from './components/Login/Login';
 
 function App() {
   const context = useContext(AuthContext);
   return (
       <React.Fragment>
         <MainHeader/>
-        
+        {!context.isLoggedIn && <Login/>}
       </React.Fragment>
   );
 }
