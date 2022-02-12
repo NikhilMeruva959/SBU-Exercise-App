97b639ffd1983c8d18721bd08ef7b44ed3ac528e
Nikhil Meruva <nikmeruva1@gmail.com>
2022-02-12 13:17:38 -0500

    Login Form, Still Working out Bugs to Transition to Home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 import React, { useContext } from 'react';
 import MainHeader from './components/Header/Header';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home'
 
 function App() {
   const context = useContext(AuthContext);
   return (
       <React.Fragment>
         <MainHeader/>
-        {!context.isLoggedIn && <Login/>}
+        <main>
+          {/* Because we are directly using the isLoggedIn in Login and Home and not just fowarding it */}
+          {!context.isLoggedIn && <Login/>}
+          {context.isLoggedIn && <Home/>}
+        </main>        
       </React.Fragment>
   );
 }
